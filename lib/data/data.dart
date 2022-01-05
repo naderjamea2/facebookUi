5db38e3d76f5742bcb1550cb430ebe23cd65cad5
nadee <naderjamea2@gmail.com>
2022-01-05 21:04:42 +0200

    main

---
+++ lib/data/data.dart
@@ -0,0 +1,51 @@
+
+
+import 'package:facebook_colne_ui/models/PostModel.dart';
+import 'package:facebook_colne_ui/models/StoryModel.dart';
+import 'package:facebook_colne_ui/models/UserModel.dart';
+
+List<UserModel> users  = [
+
+  UserModel(name: 'Nader Jamea' ,prpfileImage: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuVRgEbUUPxuyxnFw1ZPt9_LVc0LYE8uFNhA&usqp=CAU') ,
+  UserModel(name: 'Sami Ahmed' ,prpfileImage: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRY_nLFdxm5IbtARV57LQcoALiSnqRpBUKJdw&usqp=CAU') ,
+  UserModel(name: 'Ahmed Isaa' ,prpfileImage: 'https://c0.anyrgb.com/images/347/162/face-human-person-man-men-people-portrait-males-one-person-adult.jpg') ,
+  UserModel(name: 'Maged Ali' ,prpfileImage: 'https://images.alwatanvoice.com/news/images/3910365359.jpg') ,
+  UserModel(name: 'Ali Omar' ,prpfileImage: 'https://media.gemini.media/img/large/2017/8/17/2017_8_17_15_21_24_807.jpg') ,
+  UserModel(name: 'Omar Nader' ,prpfileImage: 'https://upload.wikimedia.org/wikipedia/commons/a/a0/Pierre-Person.jpg') ,
+  UserModel(name: 'Ibrahim Ameen' ,prpfileImage: 'https://upload.wikimedia.org/wikipedia/commons/a/a0/Pierre-Person.jpg') ,
+  UserModel(name: 'Yousef Breem' ,prpfileImage: 'https://upload.wikimedia.org/wikipedia/commons/a/a0/Pierre-Person.jpg') ,
+
+
+] ;
+
+
+
+
+List<PostModel> posts = [
+
+  PostModel(user:users[0] , imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLAOW4cbfpcXr7TH3UgsES6H6KwlRm_XHdNQ&usqp=CAU' , comments: 562 ,content: 'Updates his cover photo ' , likes: 766 )  ,
+  PostModel(user:users[1] , imageUrl: 'https://blog.hubspot.com/hubfs/1-Jun-23-2021-02-59-36-39-PM.jpg' , comments: 847 ,content: 'had a photoshoot session ' , likes: 848 )  ,
+  PostModel(user:users[2] , imageUrl: 'https://www.lifewire.com/thmb/yEltTeBdI3BLPNkqDrp-4fH0uf0=/3000x2000/filters:fill(auto,1)/see-previously-liked-photo-and-video-posts-4057657-c295f35c0f234a318126c52a8413ff49.png' , comments: 12 ,content: 'The clint share this picture ' , likes: 13 )  ,
+  PostModel(user:users[2] , imageUrl: 'https://buyinstagramfollowers.org/blog/wp-content/uploads/2019/05/how-to-see-someone%E2%80%99s-like-history-on-Instagram1.png' , comments: 364 ,content: 'Updates his cover photo ' , likes: 143 )  ,
+  PostModel(user:users[3] , imageUrl: 'https://img.republicworld.com/republic-prod/stories/promolarge/xhdpi/njjdgqgiklii1xaq_1588815718.jpeg' , comments: 839 ,content: 'Anniversary shared by Noor ' , likes: 156 )  ,
+  PostModel(user:users[5] , imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUzCw6COo65IKws8HItxnuCc5BH7t5wOmwuw&usqp=CAU' , comments: 250 ,content: 'The post was published ' , likes: 187 )  ,
+  PostModel(user:users[4] , imageUrl: 'https://blogs.constantcontact.com/wp-content/uploads/2013/05/Blog-header.jpg' , comments: 959 ,content: ' Updates his cover photo' , likes: 375)  ,
+  PostModel(user:users[6] , imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSi9Au4kI1MDK89jE28pV7EPgudelWfF5OIkw&usqp=CAU' , comments: 200 ,content: 'had a photo session' , likes: 947 )  ,
+
+
+
+
+] ;
+
+
+
+List<StoryModel> stories = [
+
+  StoryModel(user: users[0] , imageUrl: 'https://i.insider.com/5d8e31dd6f24eb4aa558f79a?width=1000&format=jpeg&auto=webp') ,
+  StoryModel(user: users[1] , imageUrl: 'https://media.istockphoto.com/photos/depression-in-nature-picture-id518427632') ,
+  StoryModel(user: users[2] , imageUrl: 'https://helpx.adobe.com/content/dam/help/en/photoshop/how-to/compositing/jcr%3acontent/main-pars/image/compositing_1408x792.jpg') ,
+  StoryModel(user: users[3] , imageUrl: 'https://media.istockphoto.com/photos/travel-photographer-man-taking-nature-video-of-sunset-landscape-picture-id1148282749?s=612x612') ,
+  StoryModel(user: users[4] , imageUrl: 'https://thumbs.dreamstime.com/z/betrayal-treachery-concept-silhouette-sad-man-knives-back-sunset-background-84243769.jpg') ,
+  StoryModel(user: users[5] , imageUrl: 'https://st4.depositphotos.com/3343629/19826/i/600/depositphotos_198267618-stock-photo-man-big-scissors-his-hands.jpg') ,
+  StoryModel(user: users[6] , imageUrl: 'https://static8.depositphotos.com/1008939/1016/i/600/depositphotos_10167351-stock-photo-competition.jpg') ,
+] ;
